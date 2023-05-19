2288637dccf7bfcb26b680db21dd6bd65a27179a
lmkawakami <lmkawakami@hotmail.com>
2023-05-19 02:23:13 -0300

    add unit tests summary badge

---
+++ .github/workflows/scripts/unit_test_summary.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("./html_reports/code_coverage/unit_test.xml")
+root = tree.getroot()
+node = root[0]
+errors = int(node.get("errors"))
+failures = int(node.get("failures"))
+skipped = int(node.get("skipped"))
+tests = int(node.get("tests"))
+
+print(f"tests:{tests} | failures:{failures} | errors:{errors} | skipped:{skipped}")
