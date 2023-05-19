992228a52d8a3550d74ccd395252f70fc71db0e5
lmkawakami <lmkawakami@hotmail.com>
2023-05-18 23:39:57 -0300

    remove files from pages branch

--- lmkawakami/python_actions/scripts/mutmut_score.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse("./html_reports/mutmut/mutmut-results.xml")
-root = tree.getroot()
-failures = int(root.get("failures"))
-tests = int(root.get("tests"))
-
-score = ((tests - failures) / tests) * 100
-print(f"{score:.1f}")
