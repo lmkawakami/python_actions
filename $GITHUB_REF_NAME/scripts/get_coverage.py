ac0b6bc882a535fdf9c3bdfdf2a7b5c23e0131a5
lmkawakami <lmkawakami@hotmail.com>
2023-05-19 02:13:53 +0000

    Update pages to output generated at 08e55fe

---
+++ $GITHUB_REF_NAME/scripts/get_coverage.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+
+def main():
+    tree = ET.parse('./html_reports/unit_test/cov.xml')
+    root = tree.getroot()
+    cov_percent = float(root.attrib['line-rate'])*100
+    print(f'{cov_percent:.2f}%')
+    return f'{cov_percent:.2f}%'
+
+if __name__ == '__main__':
+    main()
